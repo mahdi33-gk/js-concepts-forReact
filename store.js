bf09ab08f905bd3066a946d6f27de456ea5ef0ee
mehedi <mehedi25122003@gmail.com>
2024-10-15 07:17:19 +0600

    fdone

--- store.js
+++ store.js
@@ -5,3 +5,43 @@ const addtolac =()=>{
 
     
 }
+
+const person = {
+    naem:'lal',
+    profession: 'traffic',
+    age: 54.,
+    address: 'shahabag',
+    mia: 'bbai'
+
+}
+// dot notation
+const prof1 =person.profession;
+
+// bracket notation
+const prof2 =person["profession"]
+const naeme = 'profession';
+const prof4 = person[naeme];
+console.log(prof2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
